b98fb62cc0f54594f5ab9f325b2787c7c23f39cd
subayadhav <suba10499@gmail.com>
2018-08-06 14:12:50 +0530

    Update and rename README.md to vowels.java

---
+++ vowels.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class vowels
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in)
+char[] letter=new int{'a','e','i','o',u'};
+char c=sc.nextInt();
+if(c==letter)
+{
+System.out.println("the given letter is vowles");
+}
+else
+{
+System.out.println("the given letter is consonants");
+}
+}
+}
